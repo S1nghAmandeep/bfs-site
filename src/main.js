8fe6c16fe6c0fed6b5bb4c400d5d52cce06fdba3
SinghAmandeep <amandeepsingh3095@gmail.com>
2024-01-25 18:04:49 +0100

    add icon

--- src/main.js
+++ src/main.js
@@ -7,10 +7,10 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 /* import font awesome icon component */
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-import { faHeart } from '@fortawesome/free-regular-svg-icons'
+import { faHeart, faRectangleXmark } from '@fortawesome/free-regular-svg-icons'
 import { faBagShopping, faMagnifyingGlass, faGaugeHigh, faCloud, faTabletScreenButton, faStopwatch, faBars } from '@fortawesome/free-solid-svg-icons'
 
-library.add(faHeart, faBagShopping, faMagnifyingGlass, faGaugeHigh, faCloud, faTabletScreenButton, faStopwatch, faBars);
+library.add(faHeart, faBagShopping, faMagnifyingGlass, faGaugeHigh, faCloud, faTabletScreenButton, faStopwatch, faBars, faRectangleXmark);
 
 createApp(App)
     .component('font-awesome-icon', FontAwesomeIcon)
